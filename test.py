fdf52e461c0c87b77e93019882fc320f7c2bd91c
Guðni Natan Gunnarsson <1493259+GudniNatan@users.noreply.github.com>
2018-10-30 11:44:00 +0000

    implement unittest

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
 from my_math_lib import *
+import unittest
 
-assert add(5,5) == 10
-assert sub(5,5) == 0
-assert multiply(5,5) == 15
+class TestStringMethods(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(4, 6), 10)
+
+    def test_sub(self):
+        self.assertTrue(sub(10, 4))
+
+    def test_multiply(self):
+        s = 'hello world'
+        self.assertEqual(multiply(5, 5), 25)
+        # check that multiply fails when multiplying together strings
+        with self.assertRaises(TypeError):
+            multiply("ab" * "cd")
+
+if __name__ == '__main__':
+    unittest.main()
